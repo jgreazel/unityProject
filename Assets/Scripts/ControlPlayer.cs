c7090741cc2dd32e42fa9c22792c9996991bd51d
Jon Greazel <jdgreazel@gmail.com>
2022-05-23 17:10:12 -0500

    change dash to run

--- Assets/Scripts/ControlPlayer.cs
+++ Assets/Scripts/ControlPlayer.cs
@@ -5,28 +5,22 @@ using UnityEngine;
 public class ControlPlayer : MonoBehaviour
 {
     public float speed = 15;
-    public float jumpSpeed = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        //rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        var speedMod = 1;
+        if (Input.GetButton("Fire1")) speedMod = 2;
+
         var horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed * speedMod);
 
         var verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
-
-        // jump is really more of a dash
-        if (Input.GetButtonDown("Jump"))
-        {
-            var newPos = new Vector3(transform.position.x + horizontalInput * jumpSpeed, transform.position.y, transform.position.z + verticalInput * jumpSpeed);
-            transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * 50);
-        }
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed * speedMod);
     }
 }
